f213f4209e805a97d26ad07394b7cfbc67d376bd
Yuya Shintani <yuya.shintani.uplus26e7@gmail.com>
2021-07-11 20:44:18 +0900

    🚧 Add Exception Class

---
+++ lib/common/exception.dart
@@ -0,0 +1,89 @@
+import 'package:meta/meta.dart';
+
+class GenericException implements Exception {
+  final ExceptionCode code;
+  final dynamic info;
+
+  GenericException({this.code = ExceptionCode.unknown, this.info});
+
+  @override
+  String toString() {
+    return '$runtimeType: ${code.value}';
+  }
+
+  String get message {
+    switch (runtimeType) {
+      case NotFoundException:
+        return '${code.value}: $info\nis not found.';
+      case NotUniqueException:
+        return '${code.value}: $info\nalready exists.';
+      case NullEmptyException:
+        return '${code.value}\nmust not be null or empty.';
+      case LengthException:
+        return '${code.value} must be $info letters or shorter.';
+      case RemovalException:
+        return code == ExceptionCode.category
+            ? 'Cannot be removed;\nthis category contains notes.'
+            : 'Cannot be removed';
+      default:
+        return 'Unknown error occurred.';
+    }
+  }
+}
+
+class NotFoundException extends GenericException {
+  NotFoundException({required ExceptionCode code, required String target})
+      : assert(target.isNotEmpty),
+        super(code: code, info: target);
+}
+
+class NotUniqueException extends GenericException {
+  NotUniqueException({required ExceptionCode code, required String value})
+      : assert(value.isNotEmpty),
+        super(code: code, info: value);
+}
+
+class NullEmptyException extends GenericException {
+  NullEmptyException({required ExceptionCode code}) : super(code: code);
+}
+
+class LengthException extends GenericException {
+  LengthException({required ExceptionCode code, required int max})
+      : assert(max > 0),
+        super(code: code, info: max);
+}
+
+class RemovalException extends GenericException {
+  RemovalException({required ExceptionCode code}) : super(code: code);
+}
+
+enum ExceptionCode {
+  unknown,
+  category,
+  categoryId,
+  categoryName,
+  note,
+  noteId,
+  noteTitle,
+}
+
+extension ExceptionCodeValue on ExceptionCode {
+  String get value {
+    switch (this) {
+      case ExceptionCode.category:
+        return 'Category';
+      case ExceptionCode.categoryId:
+        return 'Category ID';
+      case ExceptionCode.categoryName:
+        return 'Category name';
+      case ExceptionCode.note:
+        return 'Note';
+      case ExceptionCode.noteId:
+        return 'Note ID';
+      case ExceptionCode.noteTitle:
+        return 'Note title';
+      default:
+        return 'Unknown';
+    }
+  }
+}
